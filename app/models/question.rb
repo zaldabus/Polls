9f7746aab1ec17ae7420f2bf0a04e0f6d8f4d635
App Academy Student <student@appacademy.io>
2014-01-16 16:28:33 -0500

    can view the results to a question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   attr_accessible :question, :poll_id
+  validates :question, :poll_id, :presence => true
 
   belongs_to :poll,
              :class_name => "Poll",
@@ -8,4 +9,13 @@ class Question < ActiveRecord::Base
   has_many :answer_choices,
            :class_name => "AnswerChoice",
            :foreign_key => :question_id
+
+   def results
+     results_hash = Hash.new(0)
+
+     answer_choices = self.answer_choices.includes(:responses)
+     answer_choices.each { |answer_choice| results_hash [answer_choice.choice]= answer_choice.responses.count}
+
+     results_hash
+   end
 end
